d76710244cb94f2292d1c2435806276b506c051e
Alan Vieceli <alansvieceli@gmail.com>
2019-03-24 18:49:45 -0300

    Ajustando a home (inútil)

--- src/main/java/br/com/alan/loja/controllers/HomeController.java
+++ src/main/java/br/com/alan/loja/controllers/HomeController.java
@@ -1,15 +1,30 @@
 package br.com.alan.loja.controllers;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+import br.com.alan.loja.daos.ProdutoDAO;
+import br.com.alan.loja.models.Produto;
 
 @Controller
 public class HomeController {
-	
+
+	@Autowired
+	ProdutoDAO produtosDao;
+
 	@RequestMapping("/")
-	public String index() {
-		return "home";
+	public ModelAndView index() {
+		List<Produto> produtos = produtosDao.listar();
+
+		ModelAndView modelAndView = new ModelAndView("home");
+		modelAndView.addObject("produtos", produtos);
 		
+		return modelAndView;
+
 	}
 
 }
