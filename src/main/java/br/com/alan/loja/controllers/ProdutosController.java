4f608b73ca29336306d1cbf3cf10749b65302123
Alan Vieceli <alansvieceli@gmail.com>
2019-03-25 19:42:49 -0300

    Resolver JSON

--- src/main/java/br/com/alan/loja/controllers/ProdutosController.java
+++ src/main/java/br/com/alan/loja/controllers/ProdutosController.java
@@ -13,6 +13,7 @@ import org.springframework.web.bind.annotation.InitBinder;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
@@ -91,5 +92,13 @@ public class ProdutosController {
 		return modelAndView;
 
 	}
+	
+	@RequestMapping("/{id}")
+	@ResponseBody
+	public Produto detalheJSON(@PathVariable Integer id) {
+
+		return produtoDao.find(id);
+
+	}	
 
 }
