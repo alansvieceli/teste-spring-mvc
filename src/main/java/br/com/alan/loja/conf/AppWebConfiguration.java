fef833630ecebeb9f21a4b4ead60590b5e2a5ce4
Alan Vieceli <alansvieceli@gmail.com>
2019-04-06 17:07:46 -0300

    internacionalização

--- src/main/java/br/com/alan/loja/conf/AppWebConfiguration.java
+++ src/main/java/br/com/alan/loja/conf/AppWebConfiguration.java
@@ -19,11 +19,15 @@ import org.springframework.web.accept.ContentNegotiationManager;
 import org.springframework.web.client.RestTemplate;
 import org.springframework.web.multipart.MultipartResolver;
 import org.springframework.web.multipart.support.StandardServletMultipartResolver;
+import org.springframework.web.servlet.LocaleResolver;
 import org.springframework.web.servlet.ViewResolver;
 import org.springframework.web.servlet.config.annotation.DefaultServletHandlerConfigurer;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+import org.springframework.web.servlet.i18n.CookieLocaleResolver;
+import org.springframework.web.servlet.i18n.LocaleChangeInterceptor;
 import org.springframework.web.servlet.view.ContentNegotiatingViewResolver;
 import org.springframework.web.servlet.view.InternalResourceViewResolver;
 
@@ -35,92 +39,101 @@ import br.com.alan.loja.infra.FileSaver;
 import br.com.alan.loja.models.CarrinhoCompras;
 
 @EnableWebMvc
-@ComponentScan(basePackageClasses={HomeController.class, ProdutoDAO.class, FileSaver.class, CarrinhoCompras.class})
+@ComponentScan(basePackageClasses = { HomeController.class, ProdutoDAO.class, FileSaver.class, CarrinhoCompras.class })
 @EnableCaching
-public class AppWebConfiguration extends WebMvcConfigurerAdapter{
-	
+public class AppWebConfiguration extends WebMvcConfigurerAdapter {
+
 	@Bean
 	public InternalResourceViewResolver internalResourceViewResolver() {
 		InternalResourceViewResolver resolver = new InternalResourceViewResolver();
 		resolver.setPrefix("/WEB-INF/views/");
 		resolver.setSuffix(".jsp");
-		
-		//resolver.setExposeContextBeansAsAttributes(true); //todos os beans visiveis na jsp
+
+		// resolver.setExposeContextBeansAsAttributes(true); //todos os beans visiveis
+		// na jsp
 		resolver.setExposedContextBeanNames("carrinhoCompras");
-		
+
 		return resolver;
-	}	
-	
-	
+	}
+
 	@Bean
 	public MessageSource messageSource() {
-		                 
+
 		ReloadableResourceBundleMessageSource messageSource = new ReloadableResourceBundleMessageSource();
-		
+
 		messageSource.setBasename("/WEB-INF/messages");
 		messageSource.setDefaultEncoding("UTF-8");
 		messageSource.setCacheSeconds(1);
-		
+
 		return messageSource;
 	}
-	
+
 	@Bean
 	public FormattingConversionService mvcConversionService() {
 		DefaultFormattingConversionService defaultFormattingConversionService = new DefaultFormattingConversionService();
-		
+
 		DateFormatterRegistrar dateFormatterRegistrar = new DateFormatterRegistrar();
 		dateFormatterRegistrar.setFormatter(new DateFormatter("dd/MM/yyyy"));
 		dateFormatterRegistrar.registerFormatters(defaultFormattingConversionService);
-		
+
 		return defaultFormattingConversionService;
 	}
-	
+
 	@Bean
 	public MultipartResolver multipartResolver() {
 		return new StandardServletMultipartResolver();
 	}
-	
-	@Override
-    public void configureDefaultServletHandling(DefaultServletHandlerConfigurer configurer) {
-        configurer.enable();
-    }
 
-    @Override
-    public void addResourceHandlers(ResourceHandlerRegistry registry) {
-        registry.addResourceHandler("/resources/**").addResourceLocations("/resources/");
-        
-    }
-    
-    @Bean
-    public RestTemplate restTemplate() {
-    	return new RestTemplate();
-    }
-    
-    @Bean
-    public CacheManager cacheMnager() {
-    	CacheBuilder<Object, Object> cacheBuilder = CacheBuilder.newBuilder()
-    		.maximumSize(100)
-    		.expireAfterAccess(5, TimeUnit.DAYS);
-    	GuavaCacheManager guavaCacheManager = new GuavaCacheManager();
-    	guavaCacheManager.setCacheBuilder(cacheBuilder);
-    	//return new ConcurrentMapCacheManager(); // não usar esse cache em producao
-    	
-    	return guavaCacheManager;
-    }
-    
-    @Bean
-    public ViewResolver contentNegotiatorViewResolver(ContentNegotiationManager manager) {
-    	List<ViewResolver> viewResolvers = new ArrayList<>();
-    	viewResolvers.add(internalResourceViewResolver());
-    	viewResolvers.add(new JsonViewResolver());
-    	
-    	ContentNegotiatingViewResolver contentNegotiatingViewResolver = new ContentNegotiatingViewResolver();
-    	
-    	contentNegotiatingViewResolver.setViewResolvers(viewResolvers);
-    	contentNegotiatingViewResolver.setContentNegotiationManager(manager);
-    	
-    	return contentNegotiatingViewResolver;
-    	
-    }
+	@Override
+	public void configureDefaultServletHandling(DefaultServletHandlerConfigurer configurer) {
+		configurer.enable();
+	}
+
+	@Override
+	public void addResourceHandlers(ResourceHandlerRegistry registry) {
+		registry.addResourceHandler("/resources/**").addResourceLocations("/resources/");
+
+	}
+
+	@Bean
+	public RestTemplate restTemplate() {
+		return new RestTemplate();
+	}
+
+	@Bean
+	public CacheManager cacheMnager() {
+		CacheBuilder<Object, Object> cacheBuilder = CacheBuilder.newBuilder().maximumSize(100).expireAfterAccess(5,
+				TimeUnit.DAYS);
+		GuavaCacheManager guavaCacheManager = new GuavaCacheManager();
+		guavaCacheManager.setCacheBuilder(cacheBuilder);
+		// return new ConcurrentMapCacheManager(); // não usar esse cache em producao
+
+		return guavaCacheManager;
+	}
+
+	@Bean
+	public ViewResolver contentNegotiatorViewResolver(ContentNegotiationManager manager) {
+		List<ViewResolver> viewResolvers = new ArrayList<>();
+		viewResolvers.add(internalResourceViewResolver());
+		viewResolvers.add(new JsonViewResolver());
+
+		ContentNegotiatingViewResolver contentNegotiatingViewResolver = new ContentNegotiatingViewResolver();
+
+		contentNegotiatingViewResolver.setViewResolvers(viewResolvers);
+		contentNegotiatingViewResolver.setContentNegotiationManager(manager);
+
+		return contentNegotiatingViewResolver;
+
+	}
+
+	@Override
+	public void addInterceptors(InterceptorRegistry registry) {
+		registry.addInterceptor(new LocaleChangeInterceptor());
+	}
+
+	@Bean
+	public LocaleResolver localeResolver() {
+		return new CookieLocaleResolver();
+	}
 
 }
